9f10fc5836b2a88b0619944ef5c050008763ca67
dynamic-daemon-637b <zl3087@columbia.edu>
2021-05-30 20:16:40 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+    def using_push(array, string)
+     array.push(string)
+    end
+    def using_unshift(array, string)
+      array.unshift(string)
+    end
+    def using_pop(array)
+      array.pop
+    end
+    def pop_with_args(array)
+      array.pop(2)
+    end
+    def using_shift(array)
+      first = array.shift()
+      first
+    end
+    def shift_with_args(array)
+      array.shift(2)
+    end
